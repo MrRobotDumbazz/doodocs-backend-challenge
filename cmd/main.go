68337b20d1d86dfb4162380f1e9c6cb41beba321
Mr.RobotDumbazz <sansskeleton415@gmail.com>
2023-10-14 16:36:01 +0600

    Added logger and fixed config

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,15 @@
 package main
 
-import "doodocsbackendchallenge/internal/config"
+import (
+	"doodocsbackendchallenge/internal/config"
+	"doodocsbackendchallenge/internal/logger"
+
+	"golang.org/x/exp/slog"
+)
 
 func main() {
 	cfg := config.MustLoad()
+	log := logger.SetupLogger()
+	log.Info("initializing server", slog.String("address", cfg.Address))
+	log.Debug("logger debug mode enabled")
 }
